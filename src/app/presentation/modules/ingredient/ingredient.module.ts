8ea89ef1b633ba4daa9c6338aea48840e8e1432e
Julian362 <Juliangarcia362@hotmail.com>
2023-04-12 23:19:35 -0500

    ser agregan componentes a ingredient module

--- src/app/presentation/modules/ingredient/ingredient.module.ts
+++ src/app/presentation/modules/ingredient/ingredient.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IngredientComponent } from './components/ingredient/ingredient.component';
+import { CreateComponent } from './components/create/create.component';
+import { AllComponent } from './components/all/all.component';
+import { DeleteComponent } from './components/delete/delete.component';
 
 
 
 @NgModule({
   declarations: [
-    IngredientComponent
+    IngredientComponent,
+    CreateComponent,
+    AllComponent,
+    DeleteComponent
   ],
   imports: [
     CommonModule
